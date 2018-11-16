57368f74de1e994445851e6bfd888a4ec7cf4da9
ahunu <Luger.Leo@gmail.com>
2018-11-16 18:06:26 +0100

    s

--- Statistik/statFunktionen.js
+++ Statistik/statFunktionen.js
@@ -440,7 +440,7 @@ function setFont() {
         var elem = document.createElement('div');
         elem.style.width = '1in';
         document.body.appendChild(elem);
-        stFontMax = 7.5 - (parseInt($(window).innerWidth() / elem.offsetWidth * 10) / 17);
+        stFontMax = 7.5 - ($(window).innerWidth() / elem.offsetWidth * 10 / 18);
         document.body.removeChild(elem);
     }
     setStMaxFont();

f1a1efb605d488fb5a48dcb0bf97c43d560277c5
ahunu <Luger.Leo@gmail.com>
2019-04-13 19:00:02 +0200

    x

--- Abakus/AaVersion.js
+++ Abakus/AaVersion.js
@@ -1,6 +1,6 @@
 
 function getVersion() {
-    return 946;   // eine neue Versionsnummer erzwingt loadCUPS() und löscht STAT*
+    return 947;   // eine neue Versionsnummer erzwingt loadCUPS() und löscht STAT*
 }
 
 function getVersionsDatum() {

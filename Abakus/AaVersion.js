2dc963f2853a2cfc73607b47fff01ff1b35e374d
ahunu <Luger.Leo@gmail.com>
2019-07-17 01:37:20 +0200

    s

--- Abakus/AaVersion.js
+++ Abakus/AaVersion.js
@@ -1,10 +1,10 @@
 
 function getVersion() {
-    return 9677;   // eine neue Versionsnummer erzwingt loadCUPS() und löscht STAT*
+    return 9678;   // eine neue Versionsnummer erzwingt loadCUPS() und löscht STAT*
 }
 
 function getVersionsDatum() {
-    return setVersion(2019, 7, 16);   // 01 - 09 !!! Oktale Darstellung ist nicht erlaubt !!!
+    return setVersion(2019, 7, 17);   // 01 - 09 !!! Oktale Darstellung ist nicht erlaubt !!!
     function setVersion(pJahr, pMonat, pTag) {
         return new Date(pJahr, (pMonat - 1), pTag);
     }

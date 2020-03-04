a3e82c9f316fb0af5faf2da12881128a825d0086
ahunu <Luger.Leo@gmail.com>
2020-03-04 11:22:09 +0100

    f

--- Abakus/AaVersion.js
+++ Abakus/AaVersion.js
@@ -1,13 +1,13 @@
 
 function getVersion() {
-    return 1032;   // eine neue Versionsnummer erzwingt loadCUPS() und löscht STAT*
+    return 1033;   // eine neue Versionsnummer erzwingt loadCUPS() und löscht STAT*
 }
 
 function getVersionsDatum() {
     function setVersion(pJahr, pMonat, pTag) {
         return new Date(pJahr, (pMonat - 1), pTag);
     }
-    return setVersion(2020, 2, 29);   // 01 - 09 !!! Oktale Darstellung ist nicht erlaubt !!!
+    return setVersion(2020, 3, 4);   // 01 - 09 !!! Oktale Darstellung ist nicht erlaubt !!!
 }
 
 

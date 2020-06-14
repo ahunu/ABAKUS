979e2fb7f6b1b2e3c020fe4063b1f4a1fa0bd9ce
ahunu <Luger.Leo@gmail.com>
2020-06-14 12:09:26 +0200

    a

--- Abakus/AaVersion.js
+++ Abakus/AaVersion.js
@@ -1,13 +1,13 @@
 
 function getVersion() {
-    return 1052;   // eine neue Versionsnummer erzwingt loadCUPS() und löscht STAT*
+    return 1053;   // eine neue Versionsnummer erzwingt loadCUPS() und löscht STAT*
 }
 
 function getVersionsDatum() {
     function setVersion(pJahr, pMonat, pTag) {
         return new Date(pJahr, (pMonat - 1), pTag);
     }
-    return setVersion(2020, 6, 12);   // 01 - 09 !!! Oktale Darstellung ist nicht erlaubt !!!
+    return setVersion(2020, 6, 14);   // 01 - 09 !!! Oktale Darstellung ist nicht erlaubt !!!
 }
 
 //17 alten Cache löschen

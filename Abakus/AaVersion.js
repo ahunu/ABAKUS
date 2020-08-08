32c84383514f83aafd89cdfc3d7d7f6c86c45163
ahunu <Luger.Leo@gmail.com>
2020-08-08 08:48:14 +0200

    s

--- Abakus/AaVersion.js
+++ Abakus/AaVersion.js
@@ -1,13 +1,13 @@
 
 function getVersion() {
-    return 1077;   // eine neue Versionsnummer erzwingt loadCUPS() und löscht STAT*
+    return 1078;   // eine neue Versionsnummer erzwingt loadCUPS() und löscht STAT*
 }
 
 function getVersionsDatum() {
     function setVersion(pJahr, pMonat, pTag) {
         return new Date(pJahr, (pMonat - 1), pTag);
     }
-    return setVersion(2020, 8, 7);   // 01 - 09 !!! Oktale Darstellung ist nicht erlaubt !!!
+    return setVersion(2020, 8, 8);   // 01 - 09 !!! Oktale Darstellung ist nicht erlaubt !!!
 }
 
 //  Tarock Meisterklasse

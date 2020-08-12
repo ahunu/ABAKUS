2f78b6897707e91d42a2e1214e74d6122c982555
ahunu <Luger.Leo@gmail.com>
2020-08-12 18:48:48 +0200

    s

--- Abakus/AaVersion.js
+++ Abakus/AaVersion.js
@@ -1,6 +1,6 @@
 
 function getVersion() {
-    return 1081;   // eine neue Versionsnummer erzwingt loadCUPS() und löscht STAT*
+    return 1082;   // eine neue Versionsnummer erzwingt loadCUPS() und löscht STAT*
 }
 
 function getVersionsDatum() {
